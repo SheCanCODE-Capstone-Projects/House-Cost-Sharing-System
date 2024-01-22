3f174067130afb4e578b7c0503870cfeb31388e2
Titisunflower <henuwz@gmail.com>
2024-01-22 14:12:04 +0200

    Links

--- SplitShare/src/components/House.jsx
+++ SplitShare/src/components/House.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
-import './house.css'
-
+import React from 'react';
+import { useSpring, animated, config } from 'react-spring';
 import { BiBed, BiBath, BiArea } from 'react-icons/bi';
+import './house.css';
+
 const House = ({ house }) => {
-  const {image, type, Location, address, bedrooms, bathrooms, surface, price} = house;
+  const { image, type, Location, address, bedrooms, bathrooms, surface, price } = house;
+
+  const fadeInAnimation = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: config.slow,
+  });
+
+  const slideUpAnimation = useSpring({
+    transform: 'translateY(0)',
+    from: { transform: 'translateY(20px)' },
+    config: config.default,
+  });
+
+  const hoverAnimation = useSpring({
+    scale: 1.05,
+    zIndex: 1,
+    config: config.default,
+  });
+
   return (
-    <div className='bg-black text-white border border-orange-400 p-5 rounded-lg rounded-tl-[90px] w-full max-w-[352px] mx-auto cursor-pointer hover:shadow-2xl transition ml-10 mr-10 shadow-md' id='house-list'>
+    <animated.div
+      className='bg-black text-white border border-orange-400 p-5 rounded-lg rounded-tl-[90px] w-full max-w-[352px] mx-auto cursor-pointer shadow-md'
+      id='house-list'
+      style={{
+        ...fadeInAnimation,
+        ...slideUpAnimation,
+        ...hoverAnimation,
+        borderRadius: '20px', // Adjust the border-radius as needed
+        boxShadow: '30px 30px 50px rgba(255, 165, 0, 0.4)', // Adjust the shadow size as needed
+      }}
+    >
       <img className='mb-8' src={image} alt='' />
       <div className='mb-4 flex gap-x-2 text-sm'>
         <div className='bg-black rounded-full text-orange-500 px-3'>{type}</div>
@@ -17,30 +47,24 @@ const House = ({ house }) => {
           <div className='text-[20px]'>
             <BiBed />
           </div>
-          <div>
-            {bedrooms}
-          </div>
+          <div>{bedrooms}</div>
         </div>
         <div className='flex items-center text-gray-400 gap-1'>
           <div className='text-[20px]'>
             <BiBath />
           </div>
-          <div>
-            {bathrooms}
-          </div>
+          <div>{bathrooms}</div>
         </div>
         <div className='flex items-center text-gray-400 gap-1'>
           <div className='text-[20px]'>
             <BiArea />
           </div>
-          <div>
-            {surface}
-          </div>
+          <div>{surface}</div>
         </div>
       </div>
       <div className='text-lg font-semibold text-white mb-4'>{price}</div>
-    </div>
-  )
-}
+    </animated.div>
+  );
+};
 
-export default House
+export default House;
